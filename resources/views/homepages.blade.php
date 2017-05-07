b0c6f8ac34582ffaf1e63a6b1ca224ee9f6e9e9a
Hackerstudent98 <lacaotungchao@gmail.com>
2017-05-07 10:12:27 +0700

    Commit message

---
+++ resources/views/homepages.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+        <title> HomePages </title>
+</head>
+<body>
+
+
+<script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
+<script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAgOdcWdysDPxh3M0D8VGvUXH1knHlxCos",
+        authDomain: "chat-de9ba.firebaseapp.com",
+        databaseURL: "https://chat-de9ba.firebaseio.com",
+        projectId: "chat-de9ba",
+        storageBucket: "chat-de9ba.appspot.com",
+        messagingSenderId: "1008244834502"
+    };
+    firebase.initializeApp(config);
+</script>
+</body>
+</html>
